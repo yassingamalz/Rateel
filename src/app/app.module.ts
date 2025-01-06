b96d4eac6f97b3fe72e829600c2c8da5f49ff983
yassingamalz <yassen.gamal.yg@gmail.com>
2025-01-06 17:46:27 +0000

    feat(deps): add fontawesome icons support - Install fontawesome core and svg icons - Add angular-fontawesome module - Set up for Islamic/Arabic UI icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LessonProgressComponent } from './features/lessons/components/lesson-pr
 import { LessonHeaderComponent } from './features/lessons/components/lesson-header/lesson-header.component';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from './layout/layout.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { LayoutModule } from './layout/layout.module';
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
-    CommonModule 
+    CommonModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
