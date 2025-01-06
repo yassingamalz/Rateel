345062a71077d9d64be0da7d9164d4d713ea2a83
yassingamalz <yassen.gamal.yg@gmail.com>
2025-01-06 17:44:13 +0000

    feat(layout): implement initial RTL main layout and sidebar - Add main layout structure with RTL support - Create collapsible sidebar component - Configure gradient and transitions

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+// sidebar.component.ts
+import { Component, Output, EventEmitter } from '@angular/core';
+import { NavigationService } from '../../core/services/navigation.service';
+import { MenuItem } from '../../shared/interfaces/menu-item';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,5 +11,16 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  @Output() collapse = new EventEmitter<boolean>();
+  isCollapsed = false;
+  menuItems: MenuItem[];
 
+  constructor(private navigationService: NavigationService) {
+    this.menuItems = this.navigationService.menuItems;
+  }
+
+  toggleSidebar(): void {
+    this.isCollapsed = !this.isCollapsed;
+    this.collapse.emit(this.isCollapsed);
+  }
 }
